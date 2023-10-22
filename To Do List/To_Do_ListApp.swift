8d83a383061567bb2701597c7fd774cdd597a397
Oleg Zhovtanskyi <o.zhovtanskyi@gmail.com>
2023-10-22 09:44:49 +0200

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Oleg Zhovtanskyi on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
