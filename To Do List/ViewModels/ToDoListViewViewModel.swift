c3371806296d846fe0825c942dd2d9b700c1ff8f
Oleg Zhovtanskyi <o.zhovtanskyi@gmail.com>
2023-10-25 11:37:06 +0200

    Enable the Swipe to Delete functionality for a user's created to do list items.

--- To Do List/ViewModels/ToDoListViewViewModel.swift
+++ To Do List/ViewModels/ToDoListViewViewModel.swift
@@ -5,11 +5,29 @@
 //  Created by Oleg Zhovtanskyi on 22/10/2023.
 //
 
+import FirebaseFirestore
 import Foundation
 ///ViewModel for list of items view
 ///Primary tab
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
     
-    init() {}
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    
+    /// Delete to do list item
+    /// - Parameter id: Item id to delete
+    func delete(id: String) {
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
+    }
 }
--- To Do List/Views/ToDoListView.swift
+++ To Do List/Views/ToDoListView.swift
@@ -5,21 +5,40 @@
 //  Created by Oleg Zhovtanskyi on 22/10/2023.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
     
-    private let userId: String
     
     init (userId: String) {
-        self.userId = userId
+        //users/<id>/todos/<entries>
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+        self._viewModel = StateObject(
+            wrappedValue: ToDoListViewViewModel(userId: userId)
+        )
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button(action: {
+                                viewModel.delete(id: item.id)
+                            }, label: {
+                                Text("Delete")
+                                
+                            })
+                            .tint(.red)
+                        }
+                }
+                .listStyle(.plain)
             }
             .navigationTitle("To Do List")
             .toolbar {
@@ -37,5 +56,5 @@ struct ToDoListView: View {
 }
 
 #Preview {
-    ToDoListView(userId: "")
+    ToDoListView(userId: "J8gEzf6WOwMFIQKnW0UeRqt0C0o2")
 }
